03141a82f2c30339a57153317c60c127279cab8c
Dashrath9503 <rohan9503473615@gmail.com>
2026-06-27 16:50:40 +0530

    Add Positive Negative Zero program

---
+++ src/com/logical/basic/NumberCheck.java
@@ -0,0 +1,30 @@
+package com.logical.basic;
+
+import java.util.Scanner;
+
+public class NumberCheck 
+{
+
+	public static void main(String[] args)
+	{
+	
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a Number: ");
+		int a = sc.nextInt();
+		
+		if (a>0) {
+			System.out.println(a+ " is Positive");
+			
+		} 
+		else if (a<0) {
+			System.out.println(a+" is Negative");
+		} 
+		else {
+			System.out.println(a+" is Zero");
+		}
+		sc.close();
+
+	}
+
+}
