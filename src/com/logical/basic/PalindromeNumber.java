ac4e543094ac305b7cc4ad0e8e4b02f86dd13b41
Dashrath9503 <rohan9503473615@gmail.com>
2026-06-28 17:52:03 +0530

    Add Palindrome Number program

---
+++ src/com/logical/basic/PalindromeNumber.java
@@ -0,0 +1,26 @@
+package com.logical.basic;
+
+import java.util.Scanner;
+
+public class PalindromeNumber 
+{
+
+	public static void main(String[] args) 
+	{
+	
+		Scanner sc = new Scanner(System.in);
+		
+		 System.out.print("Enter a number: ");
+	        int n = sc.nextInt(), temp = n, rev = 0;
+
+	        while (n != 0) 
+		{
+	            rev = rev * 10 + n % 10;
+	            n /= 10;
+	        }
+	        if (rev == temp)
+	            System.out.println("Palindrome");
+	        else
+	            System.out.println("Not Palindrome"); sc.close();
+	}
+}
