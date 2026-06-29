f3ab29ff6dacd7b22676eef366bd5dff3d669f29
Dashrath9503 <rohan9503473615@gmail.com>
2026-06-29 18:22:57 +0530

    Add GCD (HCF) program

---
+++ src/com/logical/basic/GCD.java
@@ -0,0 +1,21 @@
+package com.logical.basic;
+
+import java.util.*;
+
+public class GCD 
+{
+    public static void main(String[] args) 
+   {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt(), b = sc.nextInt();
+
+        while (b != 0) 
+	{
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        System.out.println("GCD = " + a); sc.close();
+    }
+}
