02c9ce4d0e8b56cc60b5df5190c65bee32423798
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-01 15:12:31 +0530

    Add Vowel or Consonant Checker program

---
+++ src/com/logical/basic/VowelConsonant.java
@@ -0,0 +1,18 @@
+package com.logical.basic;
+
+import java.util.*;
+
+public class VowelConsonant 
+{
+    public static void main(String[] args) 
+   {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = sc.next().toLowerCase().charAt(0);
+
+        if ("aeiou".indexOf(ch) != -1)
+            System.out.println("Vowel");
+        else
+            System.out.println("Consonant"); sc.close();
+    }
+}
