db0195bf0eda9eafb132f6d7ae2ce15dce4f67a1
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-02 12:08:19 +0530

    Add Reverse String program

---
+++ src/com/logic/intermediate/ReverseString.java
@@ -0,0 +1,17 @@
+package com.logic.intermediate;
+
+import java.util.*;
+
+public class ReverseString 
+{
+    public static void main(String[] args) 
+   {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String s = sc.nextLine();
+        String rev = new StringBuilder(s).reverse().toString();
+
+        System.out.println("Reversed: " + rev); sc.close();
+    }
+}
