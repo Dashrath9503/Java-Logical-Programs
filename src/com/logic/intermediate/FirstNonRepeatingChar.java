b9c1b613db26610aa7674bf97fbcaa1e7fb4f1d9
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-07 17:25:02 +0530

    Add First Non Repeating Character program

---
+++ src/com/logic/intermediate/FirstNonRepeatingChar.java
@@ -0,0 +1,24 @@
+package com.logic.intermediate;
+
+import java.util.*;
+
+public class FirstNonRepeatingChar 
+{
+    public static void main(String[] args) 
+   {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String s = sc.nextLine();
+
+        for (char ch : s.toCharArray()) 
+	{
+            if (s.indexOf(ch) == s.lastIndexOf(ch)) 
+	    {
+                System.out.println("First non-repeating character: " + ch);
+                return;
+            }
+        }
+        System.out.println("No unique character found."); 
+    }
+}
