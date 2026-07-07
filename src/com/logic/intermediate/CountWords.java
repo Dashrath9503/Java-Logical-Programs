6ddf7c70cb28be7f0922c6d99042fb548bbdf4b1
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-07 17:23:31 +0530

    Add Word Counter program

---
+++ src/com/logic/intermediate/CountWords.java
@@ -0,0 +1,16 @@
+package com.logic.intermediate;
+
+import java.util.*;
+
+public class CountWords 
+{
+    public static void main(String[] args) 
+   {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String s = sc.nextLine().trim();
+        String[] words = s.split("\\s+");
+        System.out.println("Word count = " + words.length); sc.close();
+    }
+}
