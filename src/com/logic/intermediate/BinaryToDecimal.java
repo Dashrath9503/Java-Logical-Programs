746fdc02a5a2af80aa7edf944b539974bd8f6da2
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-09 11:13:56 +0530

    Add Binary to Decimal Converter program

---
+++ src/com/logic/intermediate/BinaryToDecimal.java
@@ -0,0 +1,17 @@
+package com.logic.intermediate;
+
+import java.util.*;
+
+public class BinaryToDecimal 
+{
+    public static void main(String[] args) 
+   {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter binary number: ");
+        String bin = sc.nextLine();
+        int dec = 0, pow = 0;
+        for (int i = bin.length() - 1; i >= 0; i--)
+            dec += (bin.charAt(i) - '0') * Math.pow(2, pow++);
+        System.out.println("Decimal = " + dec); sc.close();
+    }
+}
