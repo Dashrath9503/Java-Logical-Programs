a0a92e11ee03d024068c8fe27f2d63543bc77c96
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-11 17:39:19 +0530

    Add Remove Duplicates from Array program

---
+++ src/com/logic/intermediate/RemoveDuplicates.java
@@ -0,0 +1,22 @@
+package com.logic.intermediate;
+
+import java.util.*;
+
+public class RemoveDuplicates 
+{
+    public static void main(String[] args) 
+   {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        Arrays.sort(arr);
+        System.out.print("Array without duplicates: ");
+        for (int i = 0; i < n; i++)
+            if (i == 0 || arr[i] != arr[i - 1])
+                System.out.print(arr[i] + " "); sc.close();
+    }
+}
