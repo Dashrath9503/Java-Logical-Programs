2f605c3ec8e2f180c0fcaf286111cd5f534052d9
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-13 11:24:15 +0530

    Add Hollow Pyramid Pattern program

---
+++ src/com/logical/Logical_and_Pattern/HollowPyramid.java
@@ -0,0 +1,28 @@
+package com.logical.Logical_and_Pattern;
+
+import java.util.*;
+
+public class HollowPyramid 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=i;j<n;j++) 
+            	System.out.print(" ");
+            for(int k=1;k<=2*i-1;k++)
+            {
+                if(k==1 || k==2*i-1 || i==n)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
