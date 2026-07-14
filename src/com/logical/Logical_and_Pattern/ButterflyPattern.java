308224267dbcc2e46cef4a2711241a9cdce32a3d
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-14 16:40:47 +0530

    Add Butterfly Pattern program

---
+++ src/com/logical/Logical_and_Pattern/ButterflyPattern.java
@@ -0,0 +1,35 @@
+package com.logical.Logical_and_Pattern;
+
+import java.util.*;
+
+public class ButterflyPattern 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++) 
+            	System.out.print("*");
+            for(int j=i;j<n;j++) 
+            	System.out.print("  ");
+            for(int j=1;j<=i;j++) 
+            	System.out.print("*");
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++) 
+            	System.out.print("*");
+            for(int j=i;j<n;j++) 
+            	System.out.print("  ");
+            for(int j=1;j<=i;j++) 
+            	System.out.print("*");
+            System.out.println();
+        }
+        sc.close();
+    }
+}
