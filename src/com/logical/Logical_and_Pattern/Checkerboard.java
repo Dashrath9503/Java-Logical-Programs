8191a8d438c4285e3b152a0cffc465851d82d9c8
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-14 16:42:44 +0530

    Add Checkerboard Pattern program

---
+++ src/com/logical/Logical_and_Pattern/Checkerboard.java
@@ -0,0 +1,23 @@
+package com.logical.Logical_and_Pattern;
+
+import java.util.*;
+
+public class Checkerboard 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                System.out.print((i+j)%2 + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
