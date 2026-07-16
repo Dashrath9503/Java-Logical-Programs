9495fbebc8dfea96a19cd85b76ced38e4a0d45bf
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-16 17:47:37 +0530

    Add Sum of Array Elements program

---
+++ src/com/logical/array_basic/SumArray.java
@@ -0,0 +1,17 @@
+package com.logical.array_basic;
+
+public class SumArray 
+{
+    public static void main(String[] args) 
+   {
+        int[] arr = {5, 15, 10, 0, 9};
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) 
+	{
+            sum += arr[i];
+        }
+
+        System.out.println("Sum of all elements = " + sum);
+    }
+}
