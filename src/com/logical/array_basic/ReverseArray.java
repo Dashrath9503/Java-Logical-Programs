3c4fcc9ec35a6098750e7675cbbf718fe063e342
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-23 15:05:24 +0530

    Add Reverse Array Elements program

---
+++ src/com/logical/array_basic/ReverseArray.java
@@ -0,0 +1,16 @@
+package com.logical.array_basic;
+
+public class ReverseArray 
+{
+    public static void main(String[] args) 
+   {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        System.out.println("Reversed array:");
+
+        for (int i = arr.length - 1; i >= 0; i--) 
+	{
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
