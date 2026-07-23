1d69c0e2d19bae54c0dead23ac8e5e092dd209d3
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-23 15:06:48 +0530

    Add Array Element Frequency program

---
+++ src/com/logical/array_basic/FrequencyElement.java
@@ -0,0 +1,21 @@
+package com.logical.array_basic;
+
+public class FrequencyElement 
+{
+    public static void main(String[] args) 
+    {
+        int[] arr = {2, 3, 2, 5, 2, 4, 3};
+        int key = 2;
+        int count = 0;
+
+        for (int num : arr) 
+	{
+            if (num == key)
+	    {
+                count++;
+	    }
+        }
+
+        System.out.println("Frequency of " + key + " = " + count);
+    }
+}
