4a8154063810e2da1f38a1dd2e1d28f5601ef3fe
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-24 21:59:10 +0530

    Add Linear Search program

---
+++ src/com/logical/searching_algorithms/LinearSearch.java
@@ -0,0 +1,30 @@
+package com.logical.searching_algorithms;
+
+public class LinearSearch 
+{
+    public static void main(String[] args) 
+    {  
+        int[] arr = {5, 10, 15, 20, 25};
+        int key = 20;
+        int index = -1;
+
+        for (int i = 0; i < arr.length; i++) 
+	{
+            if (arr[i] == key) 
+	    {
+                index = i;
+                break;
+            }
+        }
+
+        if (index != -1)
+	{
+            System.out.println("Element found at index " + index);
+	}
+        else
+	{
+            System.out.println("Element not found");
+	}
+    }
+}
+
