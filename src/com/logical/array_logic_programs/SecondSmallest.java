ae3d927b00758236392707e955291049b600c3bd
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-27 14:10:23 +0530

    Add Second Smallest Element program

---
+++ src/com/logical/array_logic_programs/SecondSmallest.java
@@ -0,0 +1,25 @@
+package com.logical.array_logic_programs;
+
+public class SecondSmallest 
+{
+    public static void main(String[] args) 
+    {
+        int[] arr = {9, 2, 5, 1, 7};
+        int first = Integer.MAX_VALUE, second = Integer.MAX_VALUE;
+
+        for (int num : arr) 
+	{
+            if (num < first) 
+	    {
+                second = first;
+                first = num;
+            } 
+	    else if (num < second && num != first) 
+	    {
+                second = num;
+            }
+        }
+
+        System.out.println("Second smallest element = " + second);
+    }
+}
