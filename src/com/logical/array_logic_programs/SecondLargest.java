2ce2cd52aa7eed0874baf7f14cfac9d93845a850
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-27 14:00:16 +0530

    Add Second Largest Element program

---
+++ src/com/logical/array_logic_programs/SecondLargest.java
@@ -0,0 +1,26 @@
+package com.logical.array_logic_programs;
+
+public class SecondLargest 
+{
+    public static void main(String[] args) 
+    {
+        int[] arr = {5, 8, 12, 7, 6};
+        int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE;
+
+        for (int num : arr) 
+	{
+            if (num > first) 
+	    {
+                second = first;
+                first = num;
+            } 
+	   else if (num > second && num != first) 
+	   {
+                second = num;
+            }
+        }
+
+        System.out.println("Second largest element = " + second);
+    }
+}
+
