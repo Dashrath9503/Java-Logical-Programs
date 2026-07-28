bcb3946fd85c0ae69b91fe33ea135fb229dd4916
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-29 01:32:40 +0530

    Add Move Negative Numbers to One Side program

---
+++ src/com/logical/array_logic_programs/MoveNegatives.java
@@ -0,0 +1,28 @@
+package com.logical.array_logic_programs;
+
+public class MoveNegatives {
+
+    public static void main(String[] args) 
+   {
+        int[] arr = {1, -2, 3, -4, 5, -6};
+        int j = 0;
+
+        for (int i = 0; i < arr.length; i++) 
+	{
+            if (arr[i] < 0) 
+	    {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+
+        System.out.println("After moving negatives:");
+
+        for (int num : arr)
+            System.out.print(num + " ");
+    }
+
+
+}
