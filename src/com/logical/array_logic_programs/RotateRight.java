68b3b72cc3975f47a055e16467e006af14842b4c
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-29 01:33:31 +0530

    Add Right Rotate Array by One Position program

---
+++ src/com/logical/array_logic_programs/RotateRight.java
@@ -0,0 +1,21 @@
+package com.logical.array_logic_programs;
+
+public class RotateRight 
+{
+    public static void main(String[] args) 
+   {
+        int[] arr = {1, 2, 3, 4, 5};
+        int last = arr[arr.length - 1];
+
+        for (int i = arr.length - 1; i > 0; i--)
+	{
+            arr[i] = arr[i - 1];
+	}
+        arr[0] = last;
+
+        System.out.println("After right rotation:");
+
+        for (int num : arr)
+            System.out.print(num + " ");
+    }
+}
