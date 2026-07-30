35efe2045f22d218df5aa57bce6749754b8c61c6
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-30 13:39:29 +0530

    Add Replace Elements with Squares program

---
+++ src/com/logical/array_logic_programs/SquareElements.java
@@ -0,0 +1,17 @@
+package com.logical.array_logic_programs;
+
+public class SquareElements 
+{
+    public static void main(String[] args) 
+   {
+        int[] arr = {2, 4, 6, 8};
+
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = arr[i] * arr[i];
+
+        System.out.println("Array after squaring elements:");
+
+        for (int num : arr)
+            System.out.print(num + " ");
+    }
+}
