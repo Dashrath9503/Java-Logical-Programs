3061355ea2ff959dccf543067bc23a0a52c01f9d
Dashrath9503 <rohan9503473615@gmail.com>
2026-07-31 17:00:22 +0530

    Add Minimum Subarray Sum program

---
+++ src/com/logical/array_pair_triplet_subarray_programs/MinSubarraySum.java
@@ -0,0 +1,19 @@
+package com.logical.array_pair_triplet_subarray_programs;
+
+public class MinSubarraySum 
+{
+    public static void main(String[] args) 
+   {
+        int[] arr = {3, -4, 2, -3, -1, 7, -5};
+        int minSoFar = arr[0];
+        int currSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) 
+	{
+            currSum = Math.min(arr[i], currSum + arr[i]);
+            minSoFar = Math.min(minSoFar, currSum);
+        }
+
+        System.out.println("Minimum subarray sum = " + minSoFar);
+    }
+}
