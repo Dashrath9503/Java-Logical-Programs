2e24c3027d6168abf0692ac4378d43f65f508c58
Dashrath9503 <rohan9503473615@gmail.com>
2026-08-01 20:51:48 +0530

    Add Maximum Product Subarray program

---
+++ src/com/logical/array_pair_triplet_subarray_programs/MaxProductSubarray.java
@@ -0,0 +1,22 @@
+package com.logical.array_pair_triplet_subarray_programs;
+
+public class MaxProductSubarray 
+{
+    public static void main(String[] args) 
+    {
+        int[] arr = {2, 3, -2, 4};
+        int maxProd = arr[0];
+        int minProd = arr[0];
+        int result = arr[0];
+
+        for (int i = 1; i < arr.length; i++) 
+	{
+            int temp = maxProd;
+            maxProd = Math.max(arr[i], Math.max(arr[i] * maxProd, arr[i] * minProd));
+            minProd = Math.min(arr[i], Math.min(arr[i] * temp, arr[i] * minProd));
+            result = Math.max(result, maxProd);
+        }
+
+        System.out.println("Maximum product subarray = " + result);
+    }
+}
