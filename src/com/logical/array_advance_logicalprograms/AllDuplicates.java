09d1039f7d311b6724a12e8532262ffa921fd200
Dashrath9503 <rohan9503473615@gmail.com>
2026-08-03 11:15:31 +0530

    Add Find All Duplicates program

---
+++ src/com/logical/array_advance_logicalprograms/AllDuplicates.java
@@ -0,0 +1,21 @@
+package com.logical.array_advance_logicalprograms;
+
+import java.util.*;
+
+public class AllDuplicates 
+{
+    public static void main(String[] args) 
+   {
+        int[] arr = {4, 3, 2, 7, 8, 2, 3, 1};
+        Set<Integer> set = new HashSet<>();
+        List<Integer> duplicates = new ArrayList<>();
+
+        for (int num : arr) 
+	{
+            if (!set.add(num))
+                duplicates.add(num);
+        }
+
+        System.out.println("Duplicates: " + duplicates);
+    }
+}
