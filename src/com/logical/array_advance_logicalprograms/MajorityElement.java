f24478b4efc13929e89384255107c9d63543c1d3
Dashrath9503 <rohan9503473615@gmail.com>
2026-08-03 11:16:09 +0530

    Add Majority Element (Moore's Voting Algorithm) program

---
+++ src/com/logical/array_advance_logicalprograms/MajorityElement.java
@@ -0,0 +1,38 @@
+package com.logical.array_advance_logicalprograms;
+
+public class MajorityElement 
+{
+    public static void main(String[] args) 
+   {
+        int[] arr = {2, 2, 1, 1, 2, 2, 2};
+
+        int candidate = arr[0];
+        int count = 1;
+
+        for (int i = 1; i < arr.length; i++) 
+	{
+            if (arr[i] == candidate)
+                count++;
+            else
+                count--;
+
+            if (count == 0) 
+	   {
+                candidate = arr[i];
+                count = 1;
+            }
+        }
+
+        // Verify
+
+        count = 0;
+        for (int num : arr)
+            if (num == candidate)
+                count++;
+
+        if (count > arr.length / 2)
+            System.out.println("Majority element = " + candidate);
+        else
+            System.out.println("No majority element");
+    }
+}
