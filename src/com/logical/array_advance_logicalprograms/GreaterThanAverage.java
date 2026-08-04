e93366618a991942a3cdb88b393dcce263db12ad
Dashrath9503 <rohan9503473615@gmail.com>
2026-08-04 12:02:07 +0530

    Add Count Elements Greater Than Average program

---
+++ src/com/logical/array_advance_logicalprograms/GreaterThanAverage.java
@@ -0,0 +1,23 @@
+package com.logical.array_advance_logicalprograms;
+
+public class GreaterThanAverage 
+{
+    public static void main(String[] args) 
+   {
+        int[] arr = {2, 4, 6, 8, 10};
+        int sum = 0;
+
+        for (int num : arr)
+            sum += num;
+
+        double avg = (double) sum / arr.length;
+        int count = 0;
+
+        for (int num : arr)
+            if (num > avg)
+                count++;
+
+        System.out.println("Average = " + avg);
+        System.out.println("Count > average = " + count);
+    }
+}
