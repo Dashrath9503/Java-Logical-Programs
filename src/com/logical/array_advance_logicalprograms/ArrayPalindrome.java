24d976765debbbf1feac44d799ebc6f348de8ad9
Dashrath9503 <rohan9503473615@gmail.com>
2026-08-05 11:06:46 +0530

    Add Array Palindrome Checker program

---
+++ src/com/logical/array_advance_logicalprograms/ArrayPalindrome.java
@@ -0,0 +1,21 @@
+package com.logical.array_advance_logicalprograms;
+
+public class ArrayPalindrome 
+{
+    public static void main(String[] args) 
+   {
+        int[] arr = {1, 2, 3, 2, 1};
+        boolean palindrome = true;
+
+        for (int i = 0; i < arr.length / 2; i++) 
+	{
+            if (arr[i] != arr[arr.length - 1 - i]) 
+	    {
+                palindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Array is palindrome: " + palindrome);
+    }
+}
