fbce2efa74fc76517a4b489f3b9325da1ffb8520
Dashrath9503 <rohan9503473615@gmail.com>
2026-08-05 11:06:13 +0530

    Add Replace Each Element with Next Greatest program

---
+++ src/com/logical/array_advance_logicalprograms/NextGreatest.java
@@ -0,0 +1,24 @@
+package com.logical.array_advance_logicalprograms;
+
+public class NextGreatest 
+{
+    public static void main(String[] args) 
+   {
+        int[] arr = {16, 17, 4, 3, 5, 2};
+        int max = -1;
+
+        for (int i = arr.length - 1; i >= 0; i--) 
+	{
+            int temp = arr[i];
+            arr[i] = max;
+
+            if (temp > max)
+                max = temp;
+        }
+
+        System.out.println("Array after replacement:");
+
+        for (int num : arr)
+            System.out.print(num + " ");
+    }
+}
