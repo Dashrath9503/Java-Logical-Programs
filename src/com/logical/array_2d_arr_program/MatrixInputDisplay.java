054b681f8ba55a124ed6228edcca792a8052fd3c
Dashrath9503 <rohan9503473615@gmail.com>
2026-08-06 19:34:35 +0530

    Add Matrix Input and Display program

---
+++ src/com/logical/array_2d_arr_program/MatrixInputDisplay.java
@@ -0,0 +1,24 @@
+package com.logical.array_2d_arr_program; 
+
+public class MatrixInputDisplay 
+{
+    public static void main(String[] args) 
+   {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Matrix:");
+
+        for (int[] row : matrix) 
+	{
+            for (int val : row)
+	   {
+                System.out.print(val + " ");
+	   }
+            System.out.println();
+        }
+    }
+}
