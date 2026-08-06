f8d4b81cd9bdf047c21ba4d21b5741cf16551636
Dashrath9503 <rohan9503473615@gmail.com>
2026-08-06 19:37:56 +0530

    Add Row and Column Sum program

---
+++ src/com/logical/array_2d_arr_program/MatrixRowColumnSum.java
@@ -0,0 +1,35 @@
+package com.logical.array_2d_arr_program;
+
+public class MatrixRowColumnSum 
+{
+    public static void main(String[] args) 
+    {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        for (int i = 0; i < matrix.length; i++) 
+	{
+            int rowSum = 0;
+
+            for (int j = 0; j < matrix[0].length; j++)
+	    {
+                rowSum += matrix[i][j];
+	    }
+            System.out.println("Row " + (i + 1) + " sum = " + rowSum);
+        }
+
+        for (int j = 0; j < matrix[0].length; j++) 
+	{
+            int colSum = 0;
+
+            for (int i = 0; i < matrix.length; i++)
+	   {
+                colSum += matrix[i][j];
+	   }
+            System.out.println("Column " + (j + 1) + " sum = " + colSum);
+        }
+    }
+}
