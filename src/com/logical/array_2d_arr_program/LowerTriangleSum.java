6d24ecba0ba273552405b5a8938499f7c323e538
Dashrath9503 <rohan9503473615@gmail.com>
2026-08-08 11:26:12 +0530

    Add Lower Triangle Sum program

---
+++ src/com/logical/array_2d_arr_program/LowerTriangleSum.java
@@ -0,0 +1,20 @@
+package com.logical.array_2d_arr_program;
+
+public class LowerTriangleSum 
+{
+    public static void main(String[] args) 
+   {
+        int[][] matrix = {
+            {1, 0, 0},
+            {2, 3, 0},
+            {4, 5, 6}
+        };
+        int sum = 0;
+
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; j <= i; j++)
+                sum += matrix[i][j];
+
+        System.out.println("Sum of lower triangle = " + sum);
+    }
+}
