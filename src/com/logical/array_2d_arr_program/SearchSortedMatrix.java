cd6e85c53738cf8854ffa2f640c3d44bdd504e2d
Dashrath9503 <rohan9503473615@gmail.com>
2026-08-09 23:05:16 +0530

    Add Search Element in Sorted Matrix program

---
+++ src/com/logical/array_2d_arr_program/SearchSortedMatrix.java
@@ -0,0 +1,30 @@
+package com.logical.array_2d_arr_program;
+
+public class SearchSortedMatrix 
+{
+    public static void main(String[] args) 
+   {
+        int[][] matrix = {
+            {1, 4, 7},
+            {8, 9, 10},
+            {11, 12, 15}
+        };
+        int key = 10, n = matrix.length, i = 0, j = n - 1;
+        boolean found = false;
+
+        while (i < n && j >= 0) 
+	{
+            if (matrix[i][j] == key) 
+	    {
+                found = true;
+                break;
+            }
+            if (matrix[i][j] > key)
+                j--;
+            else
+                i++;
+        }
+
+        System.out.println(found ? "Element found" : "Not found");
+    }
+}
