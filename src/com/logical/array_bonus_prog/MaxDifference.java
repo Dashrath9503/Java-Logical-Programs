18eab7ad700e6eb8a7f125849a2949d47b02c377
Dashrath9503 <rohan9503473615@gmail.com>
2026-08-15 10:19:15 +0530

    Add Maximum Difference with Valid Indices program

---
+++ src/com/logical/array_bonus_prog/MaxDifference.java
@@ -0,0 +1,17 @@
+package com.logical.array_bonus_prog;
+
+public class MaxDifference 
+{
+    public static void main(String[] args) 
+   {
+        int[] arr = {2, 3, 10, 6, 4, 8, 1};
+        int min = arr[0], maxDiff = arr[1] - arr[0];
+
+        for (int i = 1; i < arr.length; i++) 
+	{
+            maxDiff = Math.max(maxDiff, arr[i] - min);
+            min = Math.min(min, arr[i]);
+        }
+        System.out.println("Maximum Difference: " + maxDiff);
+    }
+}
