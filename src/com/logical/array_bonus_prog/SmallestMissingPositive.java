107906cbe15beae3e52e3422385b84199f622118
Dashrath9503 <rohan9503473615@gmail.com>
2026-08-15 10:17:43 +0530

    Add Smallest Missing Positive Integer program

---
+++ src/com/logical/array_bonus_prog/SmallestMissingPositive.java
@@ -0,0 +1,19 @@
+package com.logical.array_bonus_prog;
+
+import java.util.Arrays;
+
+public class SmallestMissingPositive 
+{
+    public static void main(String[] args) 
+   {
+        int[] arr = {0, -1, 3, 1};
+        Arrays.sort(arr);
+        int missing = 1;
+        for (int x : arr) 
+	{
+            if (x == missing) 
+	       missing++;
+        }
+        System.out.println("Smallest Missing Positive: " + missing);
+    }
+}
