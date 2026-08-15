17313ed7876822afa762c8e762f92a8b4827c8e5
Dashrath9503 <rohan9503473615@gmail.com>
2026-08-15 10:18:43 +0530

    Add Longest Consecutive Sequence program

---
+++ src/com/logical/array_bonus_prog/LongestConsecutive.java
@@ -0,0 +1,30 @@
+package com.logical.array_bonus_prog;
+
+import java.util.HashSet;
+
+public class LongestConsecutive 
+{
+    public static void main(String[] args) 
+   {
+        int[] arr = {100, 4, 200, 1, 3, 2};
+        HashSet<Integer> set = new HashSet<>();
+        for (int x : arr) 
+	    set.add(x);
+
+        int longest = 0;
+        for (int x : set) 
+	{
+            if (!set.contains(x - 1)) 
+	    {
+                int len = 1;
+
+                while (set.contains(x + len)) 
+		{
+		     len++;
+		}
+                longest = Math.max(longest, len);
+            }
+        }
+        System.out.println("Longest Sequence Length: " + longest);
+    }
+}
