0917b86f21bfd4742ebecdd8f770dba5092e4ee8
Dashrath9503 <rohan9503473615@gmail.com>
2026-08-17 11:27:05 +0530

    Add Find Common Elements Between Two Arrays program

---
+++ src/com/logical/array_mathematical/CommonElements.java
@@ -0,0 +1,20 @@
+package com.logical.array_mathematical;
+
+public class CommonElements 
+{
+    public static void main(String[] args) 
+   {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {3, 4, 5, 6, 7};
+
+        System.out.print("Common Elements: ");
+
+        for (int x : a) 
+	{
+            for (int y : b) 
+	   {
+                if (x == y) System.out.print(x + " ");
+            }
+        }
+    }
+}
